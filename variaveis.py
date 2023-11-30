49b626094ea8ea05db2d047b9813a4a9d499b640
pedrohdeoliveira <pedrohdeoliv3ira@gmail.com>
2023-11-30 11:47:27 -0300

    first

--- variaveis.py
+++ variaveis.py
@@ -7,5 +7,17 @@ boolean = 10.5
 ''' 
 # nomes para variavel
 
+nome = "pedro henrique de oliveira"
+print (nome)
+#mostrando o tipo
+print (type (nome))
+#função length (espaços tb conta)
+print (len(nome))
+#função Uper Case
+print (nome.upper())
+#for x in nome:
+    print(x)
 
+    print (nome[2])
 
+print(nome[::])
