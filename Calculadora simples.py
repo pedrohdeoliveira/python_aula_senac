6a3c9e038d52489c16734ecb9a5d3179f60b09f8
pedrohdeoliveira <pedrohdeoliv3ira@gmail.com>
2023-12-05 12:05:29 -0300

    first

--- Calculadora simples.py
+++ Calculadora simples.py
@@ -3,10 +3,10 @@
 #exemplo:
 # A soma dos dois números é: SOMA
 
-n1 = int(input("Digite o primeiro número : "))
-n2 = int(input("Digite o segundo número :"))
+n1 = int(input("Digite o primeiro numero : "))
+n2 = int(input("Digite o segundo numero :"))
 soma = n1+n2
-divisao = n1/n2
+#divisao = n1/n2
 #multiplicacao = n1*n2
 #subtracao = n1-n2
 print("O resultado da soma é : ", soma)
